6ceaba0213dabfb2b5118fa532ef93ee735d106f
Gerald Nunn <gerald.b.nunn@gmail.com>
2023-08-01 12:33:03 -0700

    Update bootstramp script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 ROLLOUTS_DEMO_NS=rollouts-demo-gitops
+SLEEP_SECONDS=20
 
 echo "Fetching cluster subdomain"
 export SUB_DOMAIN=$(oc get ingress.config.openshift.io cluster -n openshift-ingress -o jsonpath='{.spec.domain}')
@@ -6,9 +7,9 @@ echo "SUB_DOMAIN=${SUB_DOMAIN}"
 
 echo "Create rollouts GitOps instance"
 # echo "Create default instance of gitops operator"
-kustomize build environments/overlaps/gitops | envsubst '${SUB_DOMAIN}' | oc apply -f -
+kustomize build environments/overlays/gitops | envsubst '${SUB_DOMAIN}' | oc apply -f -
 
-echo "Pause $SLEEP_SECONDS seconds for the creation of the gitops-operator..."
+echo "Pause $SLEEP_SECONDS seconds for the creation of the ${ROLLOUTS_DEMO_NS} instance..."
 sleep $SLEEP_SECONDS
 
 echo "Waiting for deployments to start"
@@ -18,7 +19,7 @@ do
 done
 
 echo "Waiting for all pods to be created"
-deployments=(cluster argocd-dex-server argocd-redis argocd-repo-server argocd-server)
+deployments=(argocd-dex-server argocd-redis argocd-repo-server argocd-server)
 for i in "${deployments[@]}";
 do
   echo "Waiting for deployment $i";
